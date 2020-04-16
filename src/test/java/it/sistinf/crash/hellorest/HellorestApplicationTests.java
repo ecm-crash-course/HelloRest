ef87c953eb28832f5bfc477359c428edc0d5b573
Andrea Cirillo <Andrea.Cirillo@it.ibm.com>
2020-04-16 08:44:10 +0200

    First commit

---
+++ src/test/java/it/sistinf/crash/hellorest/HellorestApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sistinf.crash.hellorest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellorestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
